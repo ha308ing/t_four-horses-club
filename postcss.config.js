58d93f825835d39f5faf95e5d7287f418096bfbf
unknown <75647984+ha308ing@users.noreply.github.com>
2024-08-11 11:54:33 +0400

    chore: add fonts
    - use google fonts import instead postcss-font-magician to display
      cyrillic
    - set root font

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,3 @@
 export default {
-  plugins: {
-    "postcss-font-magician": {
-      aliases: {
-        serif: "Merriweather",
-        "sans-serif": "Golos Text",
-      },
-      variants: {
-        "sans-serif": {
-          400: [],
-          500: [],
-          600: [],
-          700: [],
-        },
-        serif: {
-          400: [],
-        },
-      },
-      foundries: ["google"],
-    },
-  },
+  plugins: {},
 };
