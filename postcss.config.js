bf96bce73fba442236ba30d62da484df3a2e1fc3
unknown <75647984+ha308ing@users.noreply.github.com>
2024-08-18 04:52:48 +0400

    chore: add media query variable
    - add postcss plugins:
      - @csstools/postcss-global-data
      - postcss-custom-media
    
    closes #12

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 export default {
   plugins: {
+    "@csstools/postcss-global-data": {
+      files: ["src/css/breakpoints.pcss"],
+    },
     "postcss-mixins": {
       mixinsDir: "src/css/mixins",
     },
     "postcss-custom-properties": {},
+    "postcss-custom-media": {},
   },
 };
